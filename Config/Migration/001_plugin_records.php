e3c03c90ce6d3e758c318d2f0e9baf0b9c0cea6c
s-nakajima <nakajimashouhei@gmail.com>
2016-05-07 11:55:52 +0900

    pluginsに新着、検索への表示有無のフィールド追加した事による修正

--- Config/Migration/001_plugin_records.php
+++ Config/Migration/001_plugin_records.php
@@ -50,6 +50,8 @@ class PluginRecords extends NetCommonsMigration {
 				'type' => 1,
 				'default_action' => 'circular_notices/index',
 				'default_setting_action' => 'circular_notice_frame_settings/edit',
+				'display_topics' => 1,
+				'display_search' => 1,
 			),
 			//英語
 			array(
@@ -60,6 +62,8 @@ class PluginRecords extends NetCommonsMigration {
 				'type' => 1,
 				'default_action' => 'circular_notices/index',
 				'default_setting_action' => 'circular_notice_frame_settings/edit',
+				'display_topics' => 1,
+				'display_search' => 1,
 			),
 		),
 		'PluginsRole' => array(
